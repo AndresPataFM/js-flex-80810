34863f07f6f7f8a5c51351a06032ebaee3c5152f
AndresPataFM <apfm21@Gmail.com>
2025-06-02 20:34:38 -0300

    inicio clase 3

---
+++ clase-3/micro3/actividad-practica-3.js
@@ -0,0 +1,40 @@
+/* Actividad práctica
+Crea un algoritmo utilizando funciones
+Consigna.
+
+Tomando como base la sintaxis para la creación de funciones crea al menos 3 de ellas donde se cumpla con un algoritmo de entrada de datos, procesamiento de la información y salida de los datos procesados.
+
+Para introducir los datos puedes apoyarte de la función prompt() mientras que para la salida utilizarás de momento alert() o console.log()
+
+Aspectos a incluir
+
+Archivo HTML y Archivo JS, referenciado en el HTML por etiqueta <script src="js/miarchivo.js"></script>, que incluya la definición de un algoritmo en JavaScript realizando las llamadas a las funciones en el orden correcto para procesamiento de la información y mostrar los resultados.
+
+Sugerencias
+
+Puedes usar funciones tradicionales así como arrow función o funciones anónimas según prefieras
+
+Ejemplo
+
+Crear una función que solicite los datos de entrada
+
+Crear una función que procese la información obtenida.
+
+Crear una función para mostrar el resultado final */
+
+/* function exponente(){
+  const numero = Number(prompt("Ingrese el número base"))
+  const potencia = parseInt(prompt("Ingrese el número de la potencia"))
+  let rta = 1 
+  if(potencia === 0){
+    return 1
+  }else if(potencia<0){
+    alert("No es tan avanzado este programa")
+    return NaN
+  }else{
+    for(let i = 0; i < potencia; i++){
+      rta *= numero
+    }
+  }
+  return rta
+} */
