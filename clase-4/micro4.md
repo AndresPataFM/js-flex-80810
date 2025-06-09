e9a960b4438b874304ddb6d5b7136d6c935a8611
AndresPataFM <apfm21@Gmail.com>
2025-06-09 20:27:38 -0300

    inicio clase 4

---
+++ clase-4/micro4.md
@@ -0,0 +1,5 @@
+Respuestas para el genialy.
+
+1 B
+2 A (pone 8 en vez de 88 la consigna)
+3 B
---
+++ clase-4/micro4/paraPensar.js
@@ -0,0 +1,5 @@
+// 1 - push
+
+// 2 - indexOf, posiblemente quisieron poner 88 en vez de 8
+
+// 3 - sort().join(', ')
