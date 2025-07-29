1bf5ba28480e908848b34f2d9947fc31aebca1d7
AndresPataFM <apfm21@Gmail.com>
2025-07-28 22:32:09 -0300

    commit final

--- clase-10/enVivo-10.js
+++ clase-10/enVivo-10.js
@@ -1,3 +1,34 @@
 // Destructuring
 // Object in, object Out
 
+const usuario = {
+  nombre: "Andrés",
+  id: 45457,
+  online: true
+}
+
+console.log(usuario.nombre)
+
+const {nombre, id, online} = usuario
+
+console.log(nombre)
+console.log(usuario.id)
+
+const numeros = [0,1,2,3]
+
+const [cero, uno, dos, tres] = numeros
+
+console.log(dos)
+
+/* function mostrarUsuario(user){
+  const {online} = user
+
+  console.log(online)
+  return user
+} */
+function mostrarUsuario({online}){
+  console.log(online)
+
+}
+
+console.log(mostrarUsuario(usuario))
